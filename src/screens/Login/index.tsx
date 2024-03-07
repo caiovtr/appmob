83b82d09f1901d88d6864928c59d975601119c79
caiovtr <caiovitorandraderosa@gmail.com>
2024-03-07 10:39:08 -0300

    pt2

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,12 +1,24 @@
-import { ImageBackground, Text, View } from "react-native"
+import { ImageBackground, Text, View, Image, TextInput} from "react-native"
 import { styles } from "./styles"
 
 export function Login(){
   const login = require('../../assets/imageBack.png')
+  const imglog = require('../../assets/imgLogin.png')
+  const iconElenco = require('../../assets/iconElenco.png')
   return (
     <ImageBackground style={styles.container} source={login}>
       <View style={styles.titulo}>
         <Text style={styles.titulo}>ALLVASCO</Text>
+        <Image style={styles.imagLog} source={imglog}></Image>
+        <View style={styles.log}>
+          <Text style={styles.log}>E-mail</Text>
+          <TextInput style={styles.input}></TextInput>
+          <Text style={styles.log}>Senha</Text>
+          <TextInput style={styles.input}></TextInput>
+        </View>
+        <View style={styles.icons}>
+          <Image style={styles.elenco} source={iconElenco}></Image>
+        </View>
       </View>
     </ImageBackground>
 
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -2,12 +2,35 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
     },
     titulo: {
-        marginTop: 100,
+        marginTop: 75,
         alignItems: "center",
         color: "#B5933B",
-        fontSize: 50
+        fontSize: 60
+    },
+    imagLog: {
+        width: 110,
+        height: 120,
+    },
+    log: {
+        marginTop: 65,
+        alignItems: "center",
+        color: "#B5933B",
+        fontSize: 30
+    },
+    input: {
+        height: 30,
+        borderWidth: 2,
+        minWidth: "60%",
+        borderColor: "#B5933B",
+        color: "#FFFFFF",
+    },
+    icons: {
+        marginTop: 100,
+    },
+    elenco: {
+        
     }
 })
